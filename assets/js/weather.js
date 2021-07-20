0aa4b6ce9ccede877ee3ef88ba8db7f8320d29fd
Sean <sdillon215@gmail.com>
2021-07-20 16:13:28 -0700

    current day api call

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,15 +1,27 @@
 // temporary hardcoded value for city
 var city = "salt lake city";
 // 5 day weather forecast api call
-var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c845404333af03f8f793eadcc58eeb29";
+var fiveApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c845404333af03f8f793eadcc58eeb29";
+var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c845404333af03f8f793eadcc58eeb29";
 
-fetch(apiUrl).then(function(response) {
+// fetch(fiveApiUrl).then(function(response) {
+//     if (response.ok) {
+//         response.json().then(function (data) {
+//             console.log(data.city.name);
+//             console.log(data.list[0].main.temp);
+//             console.log(data.list[0].wind.speed);
+//             console.log(data.list[0].main.humidity);
+//         });
+//     }
+// });
+
+fetch(currentApiUrl).then(function(response) {
     if (response.ok) {
         response.json().then(function (data) {
-            console.log(data.city.name);
-            console.log(data.list[0].main.temp);
-            console.log(data.list[0].wind.speed);
-            console.log(data.list[0].main.humidity);
+            console.log(data.name);
+            console.log(data.main.temp);
+            console.log(data.wind.speed);
+            console.log(data.main.humidity);
         });
     }
 });

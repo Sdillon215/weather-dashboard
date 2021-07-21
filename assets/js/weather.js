9e8adbb32b957e75c15748c061ed79df629b4b62
Sean <sdillon215@gmail.com>
2021-07-20 17:25:16 -0700

    search btn listener/ getWeather function

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,6 @@
-// temporary hardcoded value for city
-var city = "salt lake city";
+var searchCityEl = $("#search-city");
 // 5 day weather forecast api call
-var fiveApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c845404333af03f8f793eadcc58eeb29";
-var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c845404333af03f8f793eadcc58eeb29";
+// var fiveApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c845404333af03f8f793eadcc58eeb29";
 
 // fetch(fiveApiUrl).then(function(response) {
 //     if (response.ok) {
@@ -15,14 +13,33 @@ var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city 
 //     }
 // });
 
-fetch(currentApiUrl).then(function(response) {
-    if (response.ok) {
-        response.json().then(function (data) {
-            console.log(data.name);
-            console.log(data.main.temp);
-            console.log(data.wind.speed);
-            console.log(data.main.humidity);
-        });
-    }
-});
 
+var searchSubmitHandler = function (event) {
+    event.preventDefault();
+
+    // get value of search input
+    var city = searchCityEl.val().trim();
+
+    if (city) {
+        getWeather(city);
+    }
+};
+
+var getWeather = function (city) {
+    var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c845404333af03f8f793eadcc58eeb29";
+
+    fetch(currentApiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data.name);
+                console.log(data.main.temp);
+                console.log(data.wind.speed);
+                console.log(data.main.humidity);
+            });
+        }
+    });
+
+}
+
+// listen for click on search btn call searchSubmitHandler
+$("#search-btn").click("submit", searchSubmitHandler);

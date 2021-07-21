7162ec3067f01e9654d376f19743bb9cd153d6e6
Sean <sdillon215@gmail.com>
2021-07-20 18:30:04 -0700

    displayDailyWeather functioning

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -31,15 +31,48 @@ var getWeather = function (city) {
     fetch(currentApiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data.name);
-                console.log(data.main.temp);
-                console.log(data.wind.speed);
-                console.log(data.main.humidity);
+               
+                displayDailyWeather(data);
             });
         }
     });
 
-}
+};
+
+var dailyListEl = $("#daily-list");
+
+var displayDailyWeather = function (data) {
+    // empty ul element before creating new elements
+    $("#daily-list").empty();
+    var cityName = data.name;
+    var temp = data.main.temp;
+    var wind = data.wind.speed;
+    var humid = data.main.humidity;
+    
+    // adds city name to card-header
+    $("#city-span").text(cityName);
+
+    // creates dailyTempEl and give a value of temp
+    var dailyTempEl = document.createElement("li");
+    dailyTempEl.classList = "list-group-item";
+    dailyTempEl.textContent = "Tempurature: " + temp;
+
+    // creates dailyWindEl and give a value of wind
+    var dailyWindEl = document.createElement("li");
+    dailyWindEl.classList = "list-group-item";
+    dailyWindEl.textContent = "Wind: " + wind + " MPH";
+
+    // creates dailyHumidEl and gives a value of humid
+    var dailyHumidEl = document.createElement("li");
+    dailyHumidEl.classList = "list-group-item";
+    dailyHumidEl.textContent = "Humidity: " + humid + "%";
+
+    dailyListEl.append(dailyTempEl);
+    dailyListEl.append(dailyWindEl);
+    dailyListEl.append(dailyHumidEl);
+
+
+};
 
 // listen for click on search btn call searchSubmitHandler
 $("#search-btn").click("submit", searchSubmitHandler);
